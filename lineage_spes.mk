a7e393cc46a5141d35488c82b7bb7d30ac1a7bec
spesmynuts <rianemaster1221@gmail.com>
2025-04-12 00:37:57 +0800

    spes: Inherit my own keys & sign future builds
    Signed-off-by: spesmynuts <rianemaster1221@gmail.com>

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
+# Inherit Signed Keys
+$(call inherit-product, vendor/mine/keys/keys.mk)
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
