827ccd26bef232a7028531b4e53817a662f124ab
Yograt <107334627+Yograt@users.noreply.github.com>
2025-08-30 08:44:01 +0700

    spes: reintroduce Leica for 10.x

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@
 # Cleanup script to avoid build conflicts
 LINEAGE_SEPOLICY_COMMON_VENDOR_DEVICE="device/lineage/sepolicy/common/vendor/device.te"
 QTI_KERNEL_HEADER_LINEAGE="vendor/lineage/build/soong/Android.bp"
+LEICA_CAMERA_BRANCH="leica-5.0"
 
 # Remove duplicate SELinux declaration
 if [ -f "$LINEAGE_SEPOLICY_COMMON_VENDOR_DEVICE" ]; then
@@ -13,4 +14,9 @@ fi
 if [ -f "$QTI_KERNEL_HEADER_LINEAGE" ]; then
     echo "Cleaning lines 97–101 from $QTI_KERNEL_HEADER_LINEAGE to avoid build conflict..."
     sed -i '97,101d' "$QTI_KERNEL_HEADER_LINEAGE"
+fi
+
+if check_dir vendor/xiaomi/miuicamera; then
+    echo -e "Cloning Leica Camera vendor sources from ItzDFPlayer (branch: $LEICA_CAMERA_BRANCH)"
+    git clone https://gitlab.com/ItzDFPlayer/vendor_xiaomi_miuicamera -b $LEICA_CAMERA_BRANCH vendor/xiaomi/miuicamera
 fi
